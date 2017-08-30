0621d825af85cc9859d7985b4ca30bfc9be348c5
nicknitewolf <nicknitewolf@lineageos.org>
2017-08-30 17:00:16 +0800

    msm8916-common: Change WCNSS_qcom_cfg.ini file path
    Change-Id: I48381daba9c839379302e638cc8a3d6388563f26

--- msm8916.mk
+++ msm8916.mk
@@ -263,7 +263,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     kernel/motorola/msm8916/drivers/staging/prima/firmware_bin/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
-    kernel/motorola/msm8916/drivers/staging/prima/firmware_bin/WCNSS_qcom_cfg.ini:system/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini
+    kernel/motorola/msm8916/drivers/staging/prima/firmware_bin/WCNSS_qcom_cfg.ini:system/vendor/etc/wifi/WCNSS_qcom_cfg.ini
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
