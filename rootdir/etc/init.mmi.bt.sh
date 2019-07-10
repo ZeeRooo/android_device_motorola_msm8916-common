fe6e8df037c6b8c6b9d089eb32fd11e8c3330bda
ZeeRooo <chavesjuan400@gmail.com>
2019-07-09 22:54:17 -0300

    Set bluetooth address using btnvtool on init
    inspired on https://github.com/LineageOS/android_device_zuk_ham/commit/da95e9ef8cff177a6641b892ba826cd1964345fd

--- rootdir/etc/init.mmi.bt.sh
+++ rootdir/etc/init.mmi.bt.sh
@@ -1,3 +1,6 @@
 #!/vendor/bin/sh
+btnvtool -O
+address=$(btnvtool -p 2>&1 | awk '/--board-address: / {split($0, a, ": "); print a[2]}');
 
-setprop ro.boot.btmacaddr $(btnvtool -p 2>&1 | awk '/--board-address: / {split($0, a, ": "); print a[2]}');
+setprop ro.boot.btmacaddr ${address}
+setprop persist.service.bdroid.bdaddr ${address}
