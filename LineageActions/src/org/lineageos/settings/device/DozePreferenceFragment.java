6b2dcdb94bef70ca1e9338cd2422c8fa4d98e17f
ZeeRooo <chavesjuan400@gmail.com>
2019-10-04 23:04:09 -0300

    doze: Update AmbientDisplayConfiguration class path
    Change-Id: Ib0444acc1fd8614a9d47dbac06ac5f25cd6a5719

--- LineageActions/src/org/lineageos/settings/device/DozePreferenceFragment.java
+++ LineageActions/src/org/lineageos/settings/device/DozePreferenceFragment.java
@@ -21,7 +21,7 @@ import android.os.UserHandle;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceFragmentCompat;
 
-import com.android.internal.hardware.AmbientDisplayConfiguration;
+import android.hardware.display.AmbientDisplayConfiguration;
 
 public class DozePreferenceFragment extends PreferenceFragmentCompat {
     private final String CATEGORY_AMBIENT_DISPLAY = "ambient_display_key";
